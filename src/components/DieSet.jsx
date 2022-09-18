6bf193a166ae88f0b31c84622091fb545276fe09
Jesús Álvarez <khisus19@gmail.com>
2022-09-17 21:20:06 -0500

    Freeze die functionality done

--- src/components/DieSet.jsx
+++ src/components/DieSet.jsx
@@ -2,24 +2,51 @@ import React from "react";
 import dieArray from "../DieArray"
 
 export default function DieSet() {
-  const [die, setDie] = React.useState([
-    {
-      id: "1",
-      freeze: false,
-      number: 0
-    }
-  ])
+  const [die, setDie] = React.useState(dieArray)
+  
+  function roller() {
+    setDie(prevDieSet => {
+      return prevDieSet.map(dice => {
+        return dice.freeze === false ?
+          {
+            ...dice, 
+            number: ramdonmizer()
+          } :
+          dice
+      })
+    })
+  }
+  
+  function ramdonmizer() {
+    return Math.floor(Math.random() * 6) + 1
+  }
 
-  const dieElements = dieArray.map(dice => {
-    return <div key={dice.id} className="dice" >{dice.number}</div>
+  function freezer(event, id) {
+    event.target.className += " frozen"
+    setDie(prevDieSet => {
+      return prevDieSet.map(dice => {
+        return dice.id === id ? 
+          {...dice,
+            freeze: true
+          } :
+          dice
+      })
+    })
+  }
+  
+  const dieElements = die.map(dice => {
+    return <div 
+              key={dice.id} 
+              className="dice" 
+              onClick={(event) => freezer(event, dice.id)}
+            >{dice.number}</div>
   })
-
   return(
     <>
       <section className="die-grid">
         {dieElements}
       </section>
-      <button className="btn">Roll</button>
+      <button className="btn" onClick={roller}>Roll</button>
     </>
   )
 }
